f23c603a3811256e79d713fbe19b90f792473795
Phi <pton@uw.edu>
2021-12-07 08:18:36 -0800

    Finished day 7

--- day7/part1.rb
+++ day7/part1.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
-def main()
+def main
   crabs = File.read('input.txt').split(',').map(&:to_i)
-
+  median = find_least_moves(crabs)
+  moves = calc_moves(crabs, median)
+  puts "Median: #{median}, fuel: #{moves}"
 end
 
+# Takes an array of crabs and finds the median.
+def find_least_moves(crabs)
+  arr = crabs.sort
+  len = arr.length
+  (arr[(len - 1) / 2] + arr[len / 2]) / 2
+end
+
+# Finds how many moves it will take to move all the crabs to the median position.
+def calc_moves(crabs, median)
+  sum = 0
+  crabs.each { |crab| sum += (median - crab).abs }
+  sum
+end
 
 main
--- day7/part2.rb
+++ day7/part2.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
-def main()
+def main
   crabs = File.read('input.txt').split(',').map(&:to_i)
-
+  mean = find_least_moves(crabs)
+  mean = [mean.ceil, mean.floor].min # Try both positions to account for error.
+  moves = calc_moves(crabs, mean)
+  puts "Mean: #{mean}, fuel: #{moves}"
 end
 
+# Takes an array of crabs and finds the mean.
+def find_least_moves(crabs)
+  crabs.sum(0.0) / crabs.length
+end
+
+# Finds how many moves it will take to move all the crabs to the mean position.
+def calc_moves(crabs, mean)
+  sum = 0
+  crabs.each { |crab| sum += triangle((mean - crab).abs) }
+  sum
+end
+
+# Calculate the triangular number for a distance.
+def triangle(num)
+  (num * (num + 1)) / 2
+end
 
 main
