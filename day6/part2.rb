ebfad6dffbcafe2702ae6fb4bff5efb92fd69622
Phi <pton@uw.edu>
2021-12-07 07:22:52 -0800

    Added day 7, removed benchmark from day 6 part 2

--- day6/part2.rb
+++ day6/part2.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'benchmark'
-
 RESET_FISH = 6
 NEW_FISH = 8
 
---
+++ day7/part1.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+def main()
+  crabs = File.read('input.txt').split(',').map(&:to_i)
+
+end
+
+
+main
