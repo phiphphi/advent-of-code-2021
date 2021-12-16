fc380a6ea9dadbedc62f3f15ad3c093986958430
Phi <pton@uw.edu>
2021-12-16 09:05:32 -0800

    Finished day 16 (packet processing)

--- day16/part1.rb
+++ day16/part1.rb
@@ -1,46 +1,80 @@
-# frozen_string_literal: true
-
 def main
-  bits = File.read('input.txt')
+  bits = hex_to_bin(File.read('input.txt'))
 
-  p '100'
-  p process(bits)
+  # Handle outer packet.
+  result, _ = process_single(bits, 0)
+  p result
 end
 
 def hex_to_bin(hex)
-  hex.hex.to_s(2).rjust(hex.size*4, '0')
+  hex.hex.to_s(2).rjust(hex.size * 4, '0')
 end
 
-# Work through the packed hexadecimals.
-def process(bits)
-  bits = hex_to_bin(bits).chars
-  version_sum = 0
-  i = 0
+# Process a single packet.
+def process_single(bits, start)
+  p_version, start = get_packet_slice(bits, start, 3)
 
-  # Handle each packet.
-  while i < bits.length
-    curr = bits[i]
+  p_id, start = get_packet_slice(bits, start, 3)
 
-    p_version, i = get_packet_slice(bits, i, 3)
-
-    p_id, i = get_packet_slice(bits, i, 3)
-
-    case p_id
-    when 4 # Literal value.
-
-    else # Operator.
-
-    end
-
-    i += 1
+  case p_id
+  when 4 # Literal value.
+    _, start = process_literal(bits, start)
+  else # Operator.
+    result, start = process_operator(bits, start, p_id)
+    p_version += result
   end
 
-  version_sum
+  [p_version, start]
 end
 
 # Retrieve the next bits of the packet from i to i + n in decimal form.
 def get_packet_slice(bits, i, n)
-  [bits.slice(i, i + n).to_i(2), i + n + 1]
+  slice = bits[i...i + n]
+  i += n
+  [slice.to_i(2), i]
+end
+
+# Process a literal packet value.
+def process_literal(bits, i)
+  literals = []
+
+  while bits[i] == '1'
+    literals << bits[i + 1..i + 4]
+    i += 5
+  end
+
+  # Add on last sequence after 0.
+  literals << bits[i + 1..i + 4]
+  i += 5
+  [literals.join.to_i(2), i]
+end
+
+# Process an operator packet.
+def process_operator(bits, start, operator)
+  type_id, start = get_packet_slice(bits, start, 1)
+  process_subpackets(bits, start, type_id)
+end
+
+# Gets a list of the subpackets inside this packet.
+def process_subpackets(bits, start, type_id)
+  version_sum = 0
+
+  if type_id.zero? # Subpackets in bits.
+    length, start = get_packet_slice(bits, start, 15)
+    stop = start + length
+    while start < stop
+      result, start = process_single(bits, start)
+      version_sum += result
+    end
+  else # Subpackets in length.
+    count, start = get_packet_slice(bits, start, 11)
+    count.times do
+      result, start = process_single(bits, start)
+      version_sum += result
+    end
+  end
+
+  [version_sum, start]
 end
 
 main
--- day16/part2.rb
+++ day16/part2.rb
@@ -1,8 +1,99 @@
-# frozen_string_literal: true
-
 def main
-  lines = File.readlines('input.txt', chomp: true)
+  bits = hex_to_bin(File.read('input.txt'))
 
+  # Handle outer packet.
+  result, _ = process_single(bits, 0)
+  p result
+end
+
+def hex_to_bin(hex)
+  hex.hex.to_s(2).rjust(hex.size * 4, '0')
+end
+
+# Process a single packet.
+def process_single(bits, start)
+  p_version, start = get_packet_slice(bits, start, 3)
+
+  p_id, start = get_packet_slice(bits, start, 3)
+
+  case p_id
+  when 4 # Literal value.
+    result, start = process_literal(bits, start)
+  else # Operator.
+    result, start = process_operator(bits, start, p_id)
+  end
+
+  [result, start]
+end
+
+# Retrieve the next bits of the packet from i to i + n in decimal form.
+def get_packet_slice(bits, i, n)
+  slice = bits[i...i + n]
+  i += n
+  [slice.to_i(2), i]
+end
+
+# Process a literal packet value.
+def process_literal(bits, i)
+  literals = []
+
+  while bits[i] == '1'
+    literals << bits[i + 1..i + 4]
+    i += 5
+  end
+
+  # Add on last sequence after 0.
+  literals << bits[i + 1..i + 4]
+  i += 5
+  [literals.join.to_i(2), i]
+end
+
+# Process an operator packet.
+def process_operator(bits, start, operator)
+  type_id, start = get_packet_slice(bits, start, 1)
+  packets, start = process_subpackets(bits, start, type_id)
+
+  result = 0
+  case operator
+  when 0
+    result = packets.sum
+  when 1
+    result = packets.inject(:*)
+  when 2
+    result = packets.min
+  when 3
+    result = packets.max
+  when 5
+    result = packets[0] > packets[1] ? 1 : 0
+  when 6
+    result = packets[0] < packets[1] ? 1 : 0
+  when 7
+    result = packets[0] == packets[1] ? 1 : 0
+  end
+
+  [result, start]
+end
+
+# Gets a list of the subpackets inside this packet.
+def process_subpackets(bits, start, type_id)
+  packets = []
+
+  if type_id.zero? # Subpackets in bits.
+    length, start = get_packet_slice(bits, start, 15)
+    stop = start + length
+    while start < stop
+      result, start = process_single(bits, start)
+      packets << result
+    end
+  else # Subpackets in length.
+    count, start = get_packet_slice(bits, start, 11)
+    count.times do
+      result, start = process_single(bits, start)
+      packets << result
+    end
+  end
+
+  [packets, start]
 end
 
 main
